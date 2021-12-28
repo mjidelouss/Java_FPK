7b19e502114e34b0de47a1d613b18884a96ba3c9
ABDELMAJID ELOUSSE <77199542+mjidelouss@users.noreply.github.com>
2021-12-28 23:31:11 +0100

    Delete Produits.java

--- Controle 2/exercice7/Produits.java
+++
@@ -1,8 +0,0 @@
-package exercice7; 
-import java.util.Date;
-
-public interface Produits {
-	public Date date_fin_validite = new Date();
-	public Date Date_de_peremption();
-	public int jours_restant() ;
-}
